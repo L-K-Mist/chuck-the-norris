2393e54e57ff88dee70e339f10887b4c9999cf1e
Dylan <dylan@vandenbosch.co.za>
2019-01-21 21:09:55 +0200

    Getting started . Switched linter off for flow...

--- src/store.js
+++ src/store.js
@@ -1,10 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    loading: false,
+    jokes: null
+  },
+  mutations: {}, // Evan You has mentioned the likelihood of doing away with mutations, so I'm getting used to doing without them too.
+  actions: {
+    getJokes({
+      state
+    }) {
+      state.loading = true;
+      axios.get("http://api.icndb.com/jokes/random/10").then(
+        response => {
+          state.loading = false;
+          state.jokes = response.data.value;
+        },
+        error => {
+          console.log("​error", error);
+
+          this.loading = false;
+        }
+      );
+    }
+  }
 });
